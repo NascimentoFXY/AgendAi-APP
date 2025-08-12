92f5e28cc213f8b8d732ddfa4830c8933b904bf2
NascimentoFXY <gabrielnasmor@gmail.com>
2025-08-12 09:07:33 -0300

    ajustes

--- src/pages/main/Salao/modal/Agendamento/index.tsx
+++ src/pages/main/Salao/modal/Agendamento/index.tsx
@@ -71,12 +71,20 @@ export default function ScheduleModal() {
 
             <View style={styles.modalContainer}>
                 <View style={styles.SalaoInfoText}>
-                    <Text style={styles.SalaoNome}>La Mar</Text>
+                    <Text style={styles.SalaoNome}>
+                        La Mar
+                    </Text>
                     <Text style={styles.SalaoSubTitle}>Cortes de Cabelo, Maquiagem, Massagem</Text>
                 </View>
                 <View style={styles.SalaoLocContainer}>
-                    <View style={styles.SalaoLocText}><MaterialIcons name='location-on' size={20} color={colors.primary} /><Text> Rua Euclides Zalgo, Endereço 0293</Text></View>
-                    <View style={styles.SalaoLocText}><FontAwesome5 name='clock' size={20} color={colors.primary} /><Text>30 min  3.8km*Sab Dom | 06 da manhã - 11 da tarde</Text></View>
+                    <View style={styles.SalaoLocText}>
+                        <MaterialIcons name='location-on' size={20} color={colors.primary} />
+                        <Text> Rua Euclides Zalgo, Endereço 0293</Text>
+                    </View>
+                    <View style={styles.SalaoLocText}>
+                        <FontAwesome5 name='clock' size={20} color={colors.primary} />
+                        <Text>30 min  3.8km*Sab Dom | 06 da manhã - 11 da tarde</Text>
+                    </View>
                 </View>
 
             </View>
